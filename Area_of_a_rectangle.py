55d3f71af23b8a56388ed961ccb720fc6ec2d39c
Ananthakrishnan K V <ananthakrishnankv2028@cs.sjcetpalai.ac.in>
2024-10-01 21:11:18 +0530

    Program to find the area of a rectangle

---
+++ Area_of_a_rectangle.py
@@ -0,0 +1,10 @@
+'''Auther : ananthakrishnan K V
+Date : 01-10-2024
+A program to find the area of a rectangle'''
+
+length=int(input("Enter the length of the rectangle:"))
+width=int(input("Enter the width of triangel:"))
+area=length*width
+
+
+print(f"The area of the rectangle is: {area} cm")

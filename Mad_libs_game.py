7cc40dc469f156809d41e9415ab4554b6db3c15b
Ananthakrishnan K V <ananthakrishnankv2028@cs.sjcetpalai.ac.in>
2024-10-01 21:12:25 +0530

    Mad libs Game

---
+++ Mad_libs_game.py
@@ -0,0 +1,30 @@
+#Mad libs Game
+#word game where you create a story
+#by filling in blanks with random words
+
+adjective1 = input("Enter an adjective:")
+verb1 = input("Enter a verb ending in \'ing\':")
+adjective2 = input("Enter another adjective:")
+noun1 = input("Enter a noun (person,place,thing):")
+verb2 = input("Enter a verb ending in \'ing\':")
+adjective3 = input("Enter another adjective:")
+noun2 = input("Enter a noun (person,place,thing):")
+adjective4 = input("Enter another adjective:")
+adjective5 = input("Enter another adjective:")
+noun3 = input("Enter a noun (person,place,thing):")
+noun4 = input("Enter a noun (person,place,thing):")
+
+
+print(f"Our school cafeteria has really {adjective1} food.")
+print(f"Just thinking about it makes my stomach {verb1}.")
+print(f"The spaghetti is {adjective2} and taste like {noun1}")
+print(f"One day,I swer one of my meatballs started to {verb2}!")
+print(f"The turkey tacos are totally {adjective3} and look kind of like old {noun2}.")
+print(f"My friend actually like the meatloaf,even though it's {adjective4} and {adjective5}.")
+print(f"I call it \"mystery meatloaf\" and think it\'s really made out of {noun3}.")
+print(f"My dad said he\'d make lunches,but the first day,he made me a sandwitch out of {noun4} and peanut butter!")
+print(f"I think i\'d rather take my chance with the cafeteria")
+
+
+#Auther : Ananthakrishnan K V
+#Date   : 01-10-2024

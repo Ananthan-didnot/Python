1f4dfcb668e01b8ec559eddd241c69805b18e220
Ananthakrishnan K V <ananthakrishnankv2028@cs.sjcetpalai.ac.in>
2024-10-15 14:02:53 +0530

    Positive or Negative number

---
+++ stringDemo.py
@@ -0,0 +1,12 @@
+'''
+Auther:Ananthakrishnan K V
+Date:15/10/2024
+Pytjon program to check whether the given number
+is positive or negative
+'''
+
+number = int(input("Enter a a number:"))
+if number>0:
+    print(f"The number {number} is Positive")
+else:
+    print(f"The number {number} is Negative")

d4a5e6aa19377f6d60897951d6be3e73995534b9
Ananthakrishnan K V <ananthakrishnankv2028@cs.sjcetpalai.ac.in>
2024-10-15 14:51:37 +0530

    Simple_intrest

---
+++ Simple_intrest.py
@@ -0,0 +1,11 @@
+'''Auther:Ananthakrishnan K V
+Date:15/10/2024
+Python program to find the simple intrest
+'''
+
+
+p = float(input("Enter your principle amount:"))
+n = int(input("Enter number of year:"))
+r = int(input("Enter the rate of intrest(in percentage):"))
+simple_intrest = p*n*r
+print("simple intrest is",simple_intrest)

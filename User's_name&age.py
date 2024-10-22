3f5fb5217b1cc22fecefa5f1470d3f226bbf1f09
Ananthakrishnan K V <ananthakrishnankv2028@cs.sjcetpalai.ac.in>
2024-10-22 18:36:11 +0530

    Add files via upload

---
+++ User's_name&age.py
@@ -0,0 +1,11 @@
+'''
+Auther:Ananthakrishnan K V
+Date:06-10-2024
+Python program that asks the user to input their name and age.
+After receiving the input, display a message that includes the user's name and age.
+'''
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print(f"Hello,{name}! You are {age} years old.")
---
+++ operators.py
@@ -0,0 +1,29 @@
+'''
+Auther: Ananthakrishnan K V
+Date: 06-10-2024
+Program that demonstrates the usage of arithmetic, comparison, and logical operators.
+
+'''
+
+# Define two integers
+a = 10
+b = 5
+
+#arithmetic operations
+sum = a+b
+division = a/b
+
+#comparison operations
+is_greater = a>b
+are_equal = a==b
+
+# Perform logical operations
+logical_and = (a>b) and (b>0)
+logical_or = (a<b) or (b>0)
+
+# Display the results
+print(f"Sum: {sum}, Division: {division}")
+print(f"Is a greater than b?: {is_greater}")
+print(f"Are a and b equal?: {are_equal}")
+print(f"Logical AND: {logical_and}")
+print(f"Logical OR: {logical_or}")
